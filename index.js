22110bf8a8e9c800cf5f7d05c5b5d6ed1de3a1f0
siddhant-sri <sidz.dec26@gmail.com>
2024-07-15 16:08:56 +0530

    Accessing private bucket: pre signed URL for adding/modifying/PUT

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-const { S3Client, GetObjectCommand } = require("@aws-sdk/client-s3");
+const {
+  S3Client,
+  GetObjectCommand,
+  PutObjectCommand,
+} = require("@aws-sdk/client-s3");
 const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 
+// the user's credentials is of one time and is deleted from aws after use
 const s3Client = new S3Client({
   region: "ap-south-2",
   credentials: {
@@ -9,6 +14,7 @@ const s3Client = new S3Client({
   },
 });
 
+// To read the data of a private bucket
 async function getObjectURL(key) {
   const command = new GetObjectCommand({
     Bucket: "siddhantdev-private",
@@ -18,10 +24,27 @@ async function getObjectURL(key) {
   return url;
 }
 
+// To update/modify the data in a private bucket
+// NOT POST : as bucket is already created by the root user
+// POST for bucked creation
+async function putObjectURL(fileName, contentType) {
+  const command = new PutObjectCommand({
+    Bucket: "siddhantdev-private",
+    Key: `uploads/user-uploads/${fileName}`,
+    ContentType: contentType,
+  });
+  const url = await getSignedUrl(s3Client, command);
+  return url;
+}
+
 async function init() {
+  //   console.log(
+  //     "URL for dragon-neon.jpg ",
+  //     await getObjectURL("uploads/user-uploads/video-1721033665802")
+  //   );
   console.log(
-    "URL for dragon-neon.jpg ",
-    await getObjectURL("dragon-neon.jpg")
+    "URL for uplaoad ",
+    await putObjectURL(`video-${Date.now()}`, "video/mp4")
   );
 }
 
